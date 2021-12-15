e2ca6759f79b5494960ad5759429a59c043f7270
aalarie134 <91813705+aalarie134@users.noreply.github.com>
2021-12-14 22:25:14 -0500

    problem with the ending of the loop

--- src/day2.java
+++ src/day2.java
@@ -1,12 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class day2 {
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws FileNotFoundException {
         int answer;
         mover submarine = new mover();
+        File file  = new File("input.txt");
+        Scanner scan = new Scanner(file);
 
-        Scanner scan = new Scanner("input.txt");
+        while(scan.hasNextLine()) {
+            String direction = scan.next();
+            String way2 = scan.next();
+            int distance = Integer.parseInt(way2);
+            submarine.move(direction, distance);
 
+            System.out.println("direction " + direction);
+            System.out.println("distance "+ distance);
+            System.out.println(scan.hasNextLine());
+        }
+
+        System.out.println(submarine.getDepth()*submarine.getHorizontal());
     }
 }
